f0e0a6a68aa14267ba62626d45aca57fbeb6af88
agunubu <agunubu@unomaha.edu>
2025-02-10 03:12:11 +0000

    Troubleshooting errors with RPS

--- RPS.py
+++ RPS.py
@@ -1,25 +1,54 @@
-#RPS.py
-#Name:
-#Date:
-#Assignment:
 import random
 
 def main():
-  wins = 0
-  ties = 0
-  losses = 0
-  #Create a loop that continues as long as the user wants to play.
-  #User can play as many games as they wish.
+    wins = 0
+    ties = 0
+    losses = 0
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
+    playagain = 'Y'  
+    while playagain == 'Y':  # Main game loop
+        # Game logic
+          computer = random.choice(['R', 'P', 'S'])  # Computer randomly selects
+          player = input("Choose your weapon (R, P, S):\n")  # Get player's choice
 
-  #In the end, print the stats
-  print("Wins \t Ties \t Losses")
-  print("---- \t ---- \t ------")
-  print(wins, "\t", ties , "\t", losses)
+        # Print the computer's choice
+          if computer == 'R':
+                print("Computer chose Rock")
+          elif computer == 'P':
+              print("Computer chose Paper")
+          elif computer == 'S':
+              print("Computer chose Scissors")
+
+          # Determine the winner
+          if player == computer:
+              print("It's a tie.")
+              ties += 1
+          elif player == 'R' and computer == 'S':
+              print("You win!")
+              wins += 1
+          elif player == 'P' and computer == 'R':
+              print("You win!")
+              wins += 1
+          elif player == 'S' and computer == 'P':
+              print("You win!")
+              wins += 1
+          elif player == 'R' and computer == 'P':
+              print("You lose.")
+              losses += 1
+          elif player == 'P' and computer == 'S':
+              print("You lose.")
+              losses += 1
+          elif player == 'S' and computer == 'R':
+              print("You lose.")
+              losses += 1
+
+          playagain = input("Play again? (Y/N): ").upper() 
+
+    
+    print("\nGame over!")
+    print("Wins \t Ties \t Losses")
+    print("---- \t ---- \t ------")
+    print(wins, "\t", ties , "\t", losses)
 
 if __name__ == '__main__':
-  main()
+    main()
--- TempConvert.py
+++ TempConvert.py
@@ -1,17 +1,16 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name: Aurora Gunubu  
+#Date: 2/9/25
+#Assignment: Lab 3 - Temperature Conversion 
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
   #Convert that temperature to celsius, rounding to 1 decimal percision
   #Output converted temperature.
-  tempF = 80
-
-  tempC = tempF / 2
-
+  tempF = int(input ("Type in a temperature (in Fahrenheit):\n"))
+  tempC = (tempF-32)/(9/5)
+  round(tempC, 2)
   print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
   main()
